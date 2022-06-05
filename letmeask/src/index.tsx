92c30f87df72164ca0ac8cf0c2d1ef0f450adc1b
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-05 01:01:51 -0300

    sass

--- letmeask/src/index.tsx
+++ letmeask/src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './services/firebase'
+
+import './services/firebase';
+
+import "./styles/global.scss";
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
