ab57d25a209cce540a4485ec0a2ee4420fd3d571
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-06 19:19:45 -0300

    add route

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import { Home } from "./pages/home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
