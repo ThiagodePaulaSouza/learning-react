b076356bf6e53dfb52dd97c17e4ec2185cbc8a30
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-08 05:11:28 -0300

    refactoring, boas praticas

--- letmeask/src/pages/home.tsx
+++ letmeask/src/pages/home.tsx
@@ -5,13 +5,11 @@ import logoImg from "../assets/images/images/logo.svg";
 import googleIconImg from "../assets/images/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { firebase, auth } from "../services/firebase";
-import { useContext } from "react";
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
   const history = useHistory();
-  const { signInWithGoogle, user } = useContext(AuthContext)
+  const { signInWithGoogle, user } = useAuth()
 
   async function handleCreateRoom() {
     if (!user) {
