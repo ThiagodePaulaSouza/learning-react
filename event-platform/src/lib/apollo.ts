6da9ffc7f91c81a0cf25ad12d53e16a8269a3df8
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-06-21 23:47:02 -0300

    rm

--- event-platform/src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nki6gd03r601xp6ztv30t5/master',
-    cache: new InMemoryCache()
-})
