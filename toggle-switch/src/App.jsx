afcf8d0221514938769fad639cbd34a3e81733cd
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-03 21:47:49 -0300

    first commit

---
+++ toggle-switch/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import {
+  Card,
+  CardHeader,
+  Switch,
+  CardContent,
+  Box,
+  Container,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  CssBaseline,
+} from "@mui/material";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function App() {
+  return <div className="App">Hello!👋</div>;
+}
+
+export default App;
