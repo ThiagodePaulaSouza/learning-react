a8eb75f6507425d80c9caa33bf8421c68c9d5cd4
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-14 21:37:09 -0300

    imutabilidade

--- lista-de-presenca/reactapp/src/Pages/Home/index.jsx
+++ lista-de-presenca/reactapp/src/Pages/Home/index.jsx
@@ -5,22 +5,35 @@ import './styles.css'
 export function Home() {
 
   const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+
+    setStudents(prevState => [...prevState, newStudent])
+  }
 
 
   return (
     <div className="container">
-      <h1>Nome: {studentName}</h1>
+      <h1>Lista de Presença</h1>
       <input 
       type="text" 
       placeholder="Digite o nome..."
       onChange={e => setStudentName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
-      <Card name="Thiago de Paula Souza" time="10:55:25" />
-      <Card name="Rodrigo" time="11:00:21" />
-      <Card name="João" time="11:30:35" />
-      <Card name="Gabriel" time="12:04:10" />
+      {
+        students.map(student => <Card name={student.name} time={student.time} />)
+      }
     </div>
   )
 }
