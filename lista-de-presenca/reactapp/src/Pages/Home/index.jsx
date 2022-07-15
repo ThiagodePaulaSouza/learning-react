34518a63b78e7a32332ce7aaff8aaeb5f3dc15bb
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-14 21:19:07 -0300

    useState

--- lista-de-presenca/reactapp/src/Pages/Home/index.jsx
+++ lista-de-presenca/reactapp/src/Pages/Home/index.jsx
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
 import { Card } from '../../components/Card'
 import './styles.css'
 
 export function Home() {
+
+  const [studentName, setStudentName] = useState('');
+
+
   return (
     <div className="container">
-      <h1>Lista de Presenças</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <h1>Nome: {studentName}</h1>
+      <input 
+      type="text" 
+      placeholder="Digite o nome..."
+      onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Thiago de Paula Souza" time="10:55:25" />
